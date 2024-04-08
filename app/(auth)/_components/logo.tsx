6499d2309c21ffd819be1cddc2c64d1732259bb5
Aman Jindal <amanjindal231@gmail.com>
2024-04-08 11:48:30 +0530

    login

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return <div className="flex flex-col items-center gap-y-4">Logo!</div>;
+};
